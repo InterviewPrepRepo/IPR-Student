7c681211307fa72eb356bca235644892d7a3861f
Minseon Song <songminseonk@gmail.com>
2023-06-09 14:50:01 -0400

    set the config for displaying correct answer

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,24 +1,47 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { ImochaService } from '../../services/imocha-service/imocha.service';
 import TestAttemptQuestion from '../../models/testAttemptQuestion';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
 import User from 'src/app/models/user';
+import { LocalStorageService } from 'angular-web-storage';
+import { Config } from 'src/app/models/iprConfig';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.scss']
 })
-export class ReportsComponent {
+export class ReportsComponent implements OnInit{
   currentQuestion : number = 1;
 
   videoUrl : string = "";
   questions : TestAttemptQuestion[] = [];
   loading: boolean = true;
-  displayCorrectAnswer: boolean = true;
+  displayCorrectAnswer: boolean;
 
-  constructor(private activeRoute : ActivatedRoute, private imocha: ImochaService, private auth: AuthService) { }
+  constructor(private imocha: ImochaService, private auth: AuthService, private local: LocalStorageService) {
+    const config = this.local.get('ipr_config') as Config;
+    if(config && config.displayCorrectAnswer) {
+      this.displayCorrectAnswer = config.displayCorrectAnswer;
+    }
+    else {
+      this.displayCorrectAnswer = true;
+    }
+  }
+
+  onDisplayAnswerClick() : void {
+    this.displayCorrectAnswer = !this.displayCorrectAnswer;
+    let config = this.local.get('ipr_config');
+    if(config) {
+      config.displayCorrectAnswer = this.displayCorrectAnswer;
+    }
+    else {
+      config = {
+        displayCorrectAnswer: this.displayCorrectAnswer
+      };
+    }
+    this.local.set('ipr_config', config);
+  }
 
   ngOnInit(): void {
     const currentUser : User = this.auth.getCurrentUser();
@@ -30,7 +53,7 @@ export class ReportsComponent {
           const firstAnsweredQ = this.questions.findIndex(q => q.questionStatus === 'Answered')
           this.switchVideo(firstAnsweredQ);
         }
-  
+
         this.loading = false;
       })
     }
