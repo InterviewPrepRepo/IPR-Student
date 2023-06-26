8db29db761996ded64ad8dfb3595a64483e3450a
jdelacruz96 <jonathan.delacruz@revature.com>
2023-06-26 12:01:11 -0400

    started route-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, RouterStateSnapshot, Routes, Router } from '@angular/router';
 import { TestAttemptReportsComponent } from './components/test-attempt-reports/test-attempt-reports.component';
 import { InvitePageComponent } from './components/invite-page/invite-page.component';
+import { AuthService } from './services/auth-service/auth.service';
+
+
+// const canActivateReport: CanActivateFn =
+//   (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+//     const router = inject(Router);
+//     if (inject(AuthService).isAuthenticated()) {
+//       return true;
+//     } else {
+//       router.navigate(['invite']);
+//       return false;
+//     }
+//   };
+
 
 const routes: Routes = [
   {
     path: 'report',
-    component: TestAttemptReportsComponent
+    component: TestAttemptReportsComponent,
+    // canActivate: [canActivateReport]
   },
   {
     path: 'invite',
