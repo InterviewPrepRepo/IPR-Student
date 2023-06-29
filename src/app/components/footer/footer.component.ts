9de091b0c8d6fa4ee9e95cded1cb3749e03acfe5
jdelacruz96 <jonathan.delacruz@revature.com>
2023-06-29 12:53:44 -0400

    more ui tweaks for footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { cibInstagram, cibTwitter, cibTiktok, cibFacebook, cibLinkedin, cibYoutube } from '@coreui/icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  icons = {cibInstagram, cibTwitter, cibTiktok, cibFacebook, cibLinkedin, cibYoutube}; 
 }
