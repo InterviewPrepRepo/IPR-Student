ba2acb964ab22bee662e438fa9daffb0ecfe3544
Minseon Song <songminseonk@gmail.com>
2023-07-13 16:58:29 -0400

    changed test id

--- src/services/invite-service/invite.service.ts
+++ src/services/invite-service/invite.service.ts
@@ -11,7 +11,7 @@ export class InviteService {
 
   constructor(private imocha: ImochaService, private auth: AuthService) { }
 
-  readonly testId : number = 1250203;
+  readonly testId : number = 1251708;
   
   onInvite(invitee_email: string, invitee_name: string, invitee_job: string, invitee_experience: number, invitee_technologies: string[]): Observable<boolean> {
     invitee_email = invitee_email.toLowerCase();
